04d6b15b333dc237c1d18eef8c94becf85b19733
Wu Jianliang <1598789070@qq.com>
2024-05-08 23:54:46 +0800

    更新GPRGNN模型代码

--- layers.py
+++ layers.py
@@ -2,8 +2,70 @@ import torch
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.utils import add_self_loops, degree
 from dataset_loader import DataLoader
-from utils import get_L
-from torch_geometric.nn import GCNConv
+import numpy as np
+from torch.nn import Parameter
+from torch_geometric.nn.conv.gcn_conv import gcn_norm
+
+
+class GPR_prop(MessagePassing):
+    """
+    propagation class for GPR_GNN
+    """
+
+    def __init__(self, K, alpha, Init, Gamma=None, bias=True, **kwargs):
+        super(GPR_prop, self).__init__(aggr='add', **kwargs)
+        self.K = K
+        self.Init = Init
+        self.alpha = alpha
+
+        assert Init in ['SGC', 'PPR', 'NPPR', 'Random', 'WS']
+        if Init == 'SGC':
+            # SGC-like
+            TEMP = 0.0 * np.ones(K + 1)
+            TEMP[-1] = 1.0
+        elif Init == 'PPR':
+            # PPR-like
+            TEMP = alpha * (1 - alpha) ** np.arange(K + 1)
+            TEMP[-1] = (1 - alpha) ** K
+        elif Init == 'NPPR':
+            # Negative PPR
+            TEMP = (alpha) ** np.arange(K + 1)
+            TEMP = TEMP / np.sum(np.abs(TEMP))
+        elif Init == 'Random':
+            # Random
+            bound = np.sqrt(3 / (K + 1))
+            TEMP = np.random.uniform(-bound, bound, K + 1)
+            TEMP = TEMP / np.sum(np.abs(TEMP))
+        elif Init == 'WS':
+            # Specify Gamma
+            TEMP = Gamma
+
+        self.temp = Parameter(torch.tensor(TEMP))
+
+    def reset_parameters(self):
+        torch.nn.init.zeros_(self.temp)
+        for k in range(self.K + 1):
+            self.temp.data[k] = self.alpha * (1 - self.alpha) ** k
+        self.temp.data[-1] = (1 - self.alpha) ** self.K
+
+    def forward(self, x, edge_index, P_tilde, edge_weight=None):
+        edge_index, norm = gcn_norm(
+            edge_index, edge_weight, num_nodes=x.size(0), dtype=x.dtype)
+
+        hidden = x * (self.temp[0])
+        for k in range(self.K):
+            # x = self.propagate(edge_index, x=x, norm=norm)
+            x = torch.matmul(P_tilde, x)
+            gamma = self.temp[k + 1]
+            hidden = hidden + gamma * x
+        return hidden
+
+    def message(self, x_j, norm):
+        return norm.view(-1, 1) * x_j
+
+    def __repr__(self):
+        return '{}(K={}, temp={})'.format(self.__class__.__name__, self.K,
+                                          self.temp)
 
 
 class Test_GCNConv(MessagePassing):
@@ -45,18 +107,17 @@ class Test_GCNConv(MessagePassing):
         return aggr_out
 
 
-class Test_GCNConv2(MessagePassing):
+class FracGCNConv(MessagePassing):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.lin = torch.nn.Linear(in_channels, out_channels)
 
-    def forward(self, x, L):
+    def forward(self, x, P_tilde):
         x = self.lin(x)
 
         I = torch.eye(x.size(0))
 
-        out = torch.matmul(2*I - L, x)
-        # out = torch.matmul(L, x)
+        out = torch.matmul(P_tilde, x)
 
         return out
 
@@ -75,30 +136,4 @@ class Test_GCNConv2(MessagePassing):
 
 
 if __name__ == '__main__':
-    # dataset = DataLoader('texas')
-    # data = dataset[0]
-    #
-    # L = get_L(edge_index=data.edge_index, num_nodes=data.num_nodes)
-    #
-    # model = Test_GCNConv(1703, 100)
-    # result = model(data.x, data.edge_index)
-    #
-    # model2 = Test_GCNConv2(1703, 100)
-    # result2 = model2(data.x, data.edge_index, L)
-
-    x = torch.tensor([[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]],
-                     dtype=torch.float)
-    edge_index = torch.tensor([[0, 0, 1, 2], [1, 2, 0, 0]])
-    L = get_L(edge_index=edge_index, num_nodes=3)
-
-    layer1 = Test_GCNConv(3, 3)
-    result1 = layer1(x, edge_index)
-    print(result1)
-
-    layer2 = Test_GCNConv2(3, 3)
-    result2 = layer2(x, edge_index, L)
-    print(result2)
-
-    layer3 = GCNConv(3, 3)
-    result3 = layer3(x, edge_index)
-    print(result3)
+    pass
--- models.py
+++ models.py
@@ -1,74 +1,10 @@
 import torch
 from torch.nn import Linear
 from torch_geometric.nn import GCNConv, ChebConv
-from layers import Test_GCNConv, Test_GCNConv2
+from layers import FracGCNConv, GPR_prop
 import torch.nn.functional as F
-from utils import get_L, get_P_tilde
+from utils import get_L_tilde, get_P_tilde
 from scipy.linalg import fractional_matrix_power
-from torch_geometric.nn.conv import MessagePassing
-from torch_geometric.nn.conv.gcn_conv import gcn_norm
-from torch.nn import Parameter
-import numpy as np
-
-
-class GPR_prop(MessagePassing):
-    """
-    propagation class for GPR_GNN
-    """
-
-    def __init__(self, K, alpha, Init, Gamma=None, bias=True, **kwargs):
-        super(GPR_prop, self).__init__(aggr='add', **kwargs)
-        self.K = K
-        self.Init = Init
-        self.alpha = alpha
-
-        assert Init in ['SGC', 'PPR', 'NPPR', 'Random', 'WS']
-        if Init == 'SGC':
-            # SGC-like
-            TEMP = 0.0 * np.ones(K + 1)
-            TEMP[-1] = 1.0
-        elif Init == 'PPR':
-            # PPR-like
-            TEMP = alpha * (1 - alpha) ** np.arange(K + 1)
-            TEMP[-1] = (1 - alpha) ** K
-        elif Init == 'NPPR':
-            # Negative PPR
-            TEMP = (alpha) ** np.arange(K + 1)
-            TEMP = TEMP / np.sum(np.abs(TEMP))
-        elif Init == 'Random':
-            # Random
-            bound = np.sqrt(3 / (K + 1))
-            TEMP = np.random.uniform(-bound, bound, K + 1)
-            TEMP = TEMP / np.sum(np.abs(TEMP))
-        elif Init == 'WS':
-            # Specify Gamma
-            TEMP = Gamma
-
-        self.temp = Parameter(torch.tensor(TEMP))
-
-    def reset_parameters(self):
-        torch.nn.init.zeros_(self.temp)
-        for k in range(self.K + 1):
-            self.temp.data[k] = self.alpha * (1 - self.alpha) ** k
-        self.temp.data[-1] = (1 - self.alpha) ** self.K
-
-    def forward(self, x, edge_index, edge_weight=None):
-        edge_index, norm = gcn_norm(
-            edge_index, edge_weight, num_nodes=x.size(0), dtype=x.dtype)
-
-        hidden = x * (self.temp[0])
-        for k in range(self.K):
-            x = self.propagate(edge_index, x=x, norm=norm)
-            gamma = self.temp[k + 1]
-            hidden = hidden + gamma * x
-        return hidden
-
-    def message(self, x_j, norm):
-        return norm.view(-1, 1) * x_j
-
-    def __repr__(self):
-        return '{}(K={}, temp={})'.format(self.__class__.__name__, self.K,
-                                          self.temp)
 
 
 class GPRGNN(torch.nn.Module):
@@ -83,6 +19,9 @@ class GPRGNN(torch.nn.Module):
         self.dprate = args.dprate
         self.dropout = args.dropout
 
+        self.P_tilde = get_P_tilde(edge_index=dataset[0].edge_index)\
+            .to(torch.device('cuda:'+str(args.device) if torch.cuda.is_available() else 'cpu'))
+
     def reset_parameters(self):
         self.prop1.reset_parameters()
 
@@ -96,11 +35,11 @@ class GPRGNN(torch.nn.Module):
         x = self.lin2(x)
 
         if self.dprate == 0.0:
-            x = self.prop1(x, edge_index)
+            x = self.prop1(x, edge_index, self.P_tilde)
             return F.log_softmax(x, dim=1)
         else:
             x = F.dropout(x, p=self.dprate, training=self.training)
-            x = self.prop1(x, edge_index)
+            x = self.prop1(x, edge_index, self.P_tilde)
             return F.log_softmax(x, dim=1)
 
 
@@ -152,12 +91,17 @@ class GCN(torch.nn.Module):
         return F.log_softmax(x, dim=1)
 
 
-class Test_GCN(torch.nn.Module):
+class FracGCN(torch.nn.Module):
     def __init__(self, dataset, args):
         super().__init__()
-        self.conv1 = Test_GCNConv(dataset.num_features, args.hidden)
-        self.conv2 = Test_GCNConv(args.hidden, dataset.num_classes)
+        self.conv1 = FracGCNConv(dataset.num_features, args.hidden)
+        self.conv2 = FracGCNConv(args.hidden, dataset.num_classes)
         self.dropout = args.dropout
+
+        self.L_tilde = get_L_tilde(edge_index=dataset[0].edge_index)\
+            .to(torch.device('cuda:'+str(args.device) if torch.cuda.is_available() else 'cpu'))
+        self.P_tilde = get_P_tilde(edge_index=dataset[0].edge_index)\
+            .to(torch.device('cuda:'+str(args.device) if torch.cuda.is_available() else 'cpu'))
 
     def reset_parameters(self):
         self.conv1.reset_parameters()
@@ -165,34 +109,10 @@ class Test_GCN(torch.nn.Module):
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        x = self.conv1(x, edge_index)
+        x = self.conv1(x=x, P_tilde=self.P_tilde)
         x = F.relu(x)
         x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.conv2(x, edge_index)
-        return F.log_softmax(x, dim=1)
-
-
-class Test_GCN2(torch.nn.Module):
-    def __init__(self, dataset, args):
-        super().__init__()
-        self.conv1 = Test_GCNConv2(dataset.num_features, args.hidden)
-        self.conv2 = Test_GCNConv2(args.hidden, dataset.num_classes)
-        self.dropout = args.dropout
-        self.L = get_L(edge_index=dataset[0].edge_index, num_nodes=dataset[0].num_nodes)
-        self.P_tilde = get_P_tilde(edge_index=dataset[0].edge_index, num_nodes=dataset[0].num_nodes)
-
-    def reset_parameters(self):
-        self.conv1.reset_parameters()
-        self.conv2.reset_parameters()
-
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-        x = self.conv1(x=x, L=self.L)
-        # x = self.conv1(x=x, L=self.P_tilde)
-        x = F.relu(x)
-        x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.conv2(x=x, L=self.L)
-        # x = self.conv2(x=x, L=self.P_tilde)
+        x = self.conv2(x=x, P_tilde=self.P_tilde)
         return F.log_softmax(x, dim=1)
 
 
